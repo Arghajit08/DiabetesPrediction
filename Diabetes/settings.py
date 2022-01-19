a135d1aac098583f9ccfb1dffab5a355668fe3dc
Arghajit08 <89945847+Arghajit08@users.noreply.github.com>
2022-01-19 21:47:39 +0530

    Update settings.py

--- Diabetes/settings.py
+++ Diabetes/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ezm)9^3&=omqbwf2#x)y$&*k&y!7jk!56h@vx803u#f9p^=$zv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','diabeteswe.herokuapp.com']
 
